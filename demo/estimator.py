3b172b712ff28847aa98d11545656aafe3104732
Grisha <grisha_gerasimov01@mail.ru>
2023-12-21 15:00:11 +0300

    Add new files DNN for Gaze using laptop camera

--- demo/estimator.py
+++ demo/estimator.py
@@ -1,48 +1,29 @@
-#!/usr/bin/env python3
-
-# --------------------------------------------------------
-# Copyright (C) 2020 NVIDIA Corporation. All rights reserved.
-# NVIDIA Source Code License (1-Way Commercial)
-# Code written by Shalini De Mello.
-# --------------------------------------------------------
-
 import cv2
-from subprocess import call
 import numpy as np
-from os import path
-import pickle
 import sys
-import os
-import torch
-
-sys.path.append("ext/eth")
-from undistorter import Undistorter
-from KalmanFilter1D import Kalman1D
 
 from face import face
-from landmarks import landmarks
-from head import PnPHeadPoseEstimator
+
 from normalization import normalize
 from frame_processor import frame_processer
-import torch.nn.functional as F
-sys.path.append("../src")
-from losses import GazeAngularLoss
-
 
 
 class estimator(frame_processer):
 
-
     def __init__(self, cam_calib):
         super().__init__(cam_calib)
-    def estimate(self, device, gaze_network, img, g_t):
 
-        img = self.undistorter.apply(img)
+    def preprocess(self, images, targets):
+        data = {'image_a': [], 'gaze_a': [], 'head_a': [], 'R_gaze_a': [], 'R_head_a': []}
 
-        # detect face
-        face_location = face.detect(img,  scale=0.25, use_max='SIZE')
+        for idx, img in enumerate(images):
+            img = self.undistorter.apply(img)
+            g_t = targets[idx]
 
-        if len(face_location) > 0:
+            face_location = face.detect(img,  scale=0.25, use_max='SIZE')
+            if len(face_location) == 0:
+                continue
+
             # use kalman filter to smooth bounding box position
             # assume work with complex numbers:
             output_tracked = self.kalman_filters[0].update(face_location[0] + 1j * face_location[1])
@@ -52,6 +33,7 @@ class estimator(frame_processer):
 
             # detect facial points
             pts = self.landmarks_detector.detect(face_location, img)
+
             # run Kalman filter on landmarks to smooth them
             for i in range(68):
                 kalman_filters_landm_complex = self.kalman_filters_landm[i].update(pts[i, 0] + 1j * pts[i, 1])
@@ -62,15 +44,10 @@ class estimator(frame_processer):
             camera_parameters = np.asarray([fx, fy, cx, cy])
             rvec, tvec = self.head_pose_estimator.fit_func(pts, camera_parameters)
 
-            ######### GAZE PART #########
-
-            # create normalized eye patch and gaze and head pose value,
-            # if the ground truth point of regard is given
             head_pose = (rvec, tvec)
             por = np.zeros((3, 1))
             por[0] = g_t[0]
             por[1] = g_t[1]
-
             entry = {
                     'full_frame': img,
                     '3d_gaze_target': por,
@@ -80,19 +57,18 @@ class estimator(frame_processer):
                                             int(face_location[2] - face_location[0]),
                                             int(face_location[3] - face_location[1]))
                     }
-            [patch, h_n, g_n, inverse_M, gaze_cam_origin, gaze_cam_target] = normalize(entry, head_pose)
-
+            patch, h_n, g_n, _, _, _ = normalize(entry, head_pose)
 
             def preprocess_image(image):
                 ycrcb = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
                 ycrcb[:, :, 0] = cv2.equalizeHist(ycrcb[:, :, 0])
                 image = cv2.cvtColor(ycrcb, cv2.COLOR_YCrCb2RGB)
+                # cv2.imshow('processed patch', image)
 
                 image = np.transpose(image, [2, 0, 1])  # CxHxW
                 image = 2.0 * image / 255.0 - 1
                 return image
 
-            # estimate the PoR using the gaze network
             processed_patch = preprocess_image(patch)
             processed_patch = processed_patch[np.newaxis, :, :, :]
 
@@ -115,43 +91,18 @@ class estimator(frame_processer):
                     [-sin_, 0., cos_]
                 ]).astype(np.float32)
 
+
             def calculate_rotation_matrix(e):
                 return np.matmul(R_y(e[1]), R_x(e[0]))
 
             R_head_a = calculate_rotation_matrix(h_n)
-            R_gaze_a = np.zeros((1, 3, 3))
-            input_dict = {
-                'image_a': processed_patch,
-                'gaze_a': g_n,
-                'head_a': h_n,
-                'R_gaze_a': R_gaze_a,
-                'R_head_a': R_head_a,
-            }
-
-            # compute eye gaze and point of regard
-            for k, v in input_dict.items():
-                input_dict[k] = torch.FloatTensor(v).to(device).detach()
-
-            gaze_network.eval()
-            output_dict = gaze_network(input_dict)
-            output = output_dict['gaze_a_hat']
-            g_cnn = output.data.cpu().numpy()
-            g_cnn = g_cnn.reshape(3, 1)
-            g_cnn /= np.linalg.norm(g_cnn)
+            R_gaze_a = calculate_rotation_matrix(g_n)
 
 
-            def pitchyaw_to_vector(pitchyaw):
-                vector = np.zeros((3, 1))
-                vector[0, 0] = np.cos(pitchyaw[0]) * np.sin(pitchyaw[1])
-                vector[1, 0] = np.sin(pitchyaw[0])
-                vector[2, 0] = np.cos(pitchyaw[0]) * np.cos(pitchyaw[1])
-                return vector
+            data['image_a'].append(processed_patch)
+            data['gaze_a'].append(g_n)
+            data['head_a'].append(h_n)
+            data['R_gaze_a'].append(R_gaze_a)
+            data['R_head_a'].append(R_head_a)
 
-            g_vector = torch.FloatTensor(pitchyaw_to_vector(g_n))
-            return nn_angular_distance(g_vector, torch.FloatTensor(g_cnn))
-
-
-def nn_angular_distance(a, b):
-    sim = F.cosine_similarity(a, b, eps=1e-6)
-    sim = F.hardtanh(sim, -1.0 + 1e-6, 1.0 - 1e-6)
-    return torch.mean(torch.acos(sim) * (180 / np.pi))
+        return data  
--- demo/run_estimator.py
+++ demo/run_estimator.py
@@ -15,19 +15,20 @@ import sys
 import torch
 import os
 import glob
+import random
 
 import warnings
 warnings.filterwarnings("ignore")
 
 from estimator import estimator
 
+sys.path.append("../src")
+from losses import GazeAngularLoss
+
 #################################
 # Load gaze network
 #################################
 ted_parameters_path = 'demo_weights/weights_ted.pth.tar'
-maml_parameters_path = 'demo_weights/weights_maml'
-
-
 
 # Set device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -51,48 +52,71 @@ gaze_network = DTED(
 # Load T-ED weights if available
 assert os.path.isfile(ted_parameters_path)
 print('> Loading: %s' % ted_parameters_path)
-ted_weights = torch.load(ted_parameters_path, map_location=torch.device('cpu'))
-#if torch.cuda.device_count() == 1:
+ted_weights = torch.load(ted_parameters_path, map_location=torch.device("cpu"))
 if next(iter(ted_weights.keys())).startswith('module.'):
-   ted_weights = dict([(k[7:], v) for k, v in ted_weights.items()])
+    ted_weights = dict([(k[7:], v) for k, v in ted_weights.items()])
 
 
 gaze_network.load_state_dict(ted_weights)
 
-# Initialize monitor and frame processor
 
-DIR_TO_SAVE = "data"
-dir_list = os.listdir(DIR_TO_SAVE)
-
+DATASET_DIR = "data"
+dir_list = os.listdir(DATASET_DIR)
 
 for dir in dir_list:
-     print("Counting loss for ", dir)
-     cam_calib = {'mtx': np.eye(3), 'dist': np.zeros((1, 5))}
+    print("Evaluating", dir)
+    
+    path = os.path.join(DATASET_DIR, dir)
 
-     path = os.path.join(DIR_TO_SAVE, dir)
+    cam_calib = {'mtx': np.eye(3), 'dist': np.zeros((1, 5))}
+    cam_calib = pickle.load(open(os.path.join(path, "calib_cam.pkl"), "rb"))
 
-     cam_calib = pickle.load(open(os.path.join("calib_cam%d.pkl" % (0)), "rb"))
+    ipaths = sorted(glob.glob(os.path.join(path, 'img_*.jpg')))
+    images = []
+    for ipath in ipaths:
+        img = cv2.imread(ipath)
+        images.append(img)
 
-     ipaths = sorted(glob.glob(os.path.join(path, 'img_*.jpg')))
-     images = []
-     for ipath in ipaths:
-         img = cv2.imread(ipath)
-         images.append(img)
+    points_path = os.path.join(path, f"points.pickle")
 
-     points_path = os.path.join(path, f"points.pickle")
+    with open(points_path, 'rb') as file:
+        points = pickle.load(file)
 
-     with open(points_path, 'rb') as file:
-         points = pickle.load(file)
+    est = estimator(cam_calib)    
+    data = est.preprocess(images, points)
 
-     est = estimator(cam_calib)
+    n = len(data['image_a'])
 
-     errors = []
+    _, c, h, w = data['image_a'][0].shape
+    img = np.zeros((n, c, h, w))
+    gaze_a = np.zeros((n, 2))
+    head_a = np.zeros((n, 2))
+    R_gaze_a = np.zeros((n, 3, 3))
+    R_head_a = np.zeros((n, 3, 3))
+    for i in range(n):
+        img[i, :, :, :] = data['image_a'][i]
+        gaze_a[i, :] = data['gaze_a'][i]
+        head_a[i, :] = data['head_a'][i]
+        R_gaze_a[i, :, :] = data['R_gaze_a'][i]
+        R_head_a[i, :, :] = data['R_head_a'][i]
 
-     for idx, image in enumerate(images):
-         errors.append(est.estimate(device, gaze_network, image, points[idx]))
+    input_dict_valid = {
+        'image_a': img,
+        'gaze_a': gaze_a,
+        'head_a': head_a,
+        'R_gaze_a': R_gaze_a,
+        'R_head_a': R_head_a,
+    }
 
-     mean_error = torch.mean(torch.FloatTensor(errors))
 
-     print(f"Mean error for {dir} is {mean_error}")
+    for k, v in input_dict_valid.items():
+        input_dict_valid[k] = torch.FloatTensor(v).to(device).detach()
 
 
+    gaze_network.eval()
+    output_dict = gaze_network(input_dict_valid)
+    
+    loss = GazeAngularLoss()
+    valid_loss = loss(input_dict_valid, output_dict).cpu()
+
+    print('Mean angular error: %.2f' % (valid_loss.item()))
